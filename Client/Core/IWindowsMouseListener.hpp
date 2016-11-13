9e5c76031cc48185283ad87776bfc5b8fabeea1f
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-13 21:02:35 +0100

    Add Windows Mouse Hook

---
+++ Client/Core/IWindowsMouseListener.hpp
@@ -0,0 +1,15 @@
+//
+// Created by etiennep on 11/8/16.
+//
+
+#ifndef SPIDER_CLIENT_IWINDOWSMOUSELISTENER_HPP
+#define SPIDER_CLIENT_IWINDOWSMOUSELISTENER_HPP
+
+class IWindowsMouseListener {
+public:
+	virtual void Run() = 0;
+	virtual std::string Flush() = 0;
+	virtual void PushMouselog(std::string &keylog) = 0;
+};
+
+#endif //SPIDER_CLIENT_IWINDOWSMOUSELISTENER_HPP
